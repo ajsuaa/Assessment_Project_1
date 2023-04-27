a8caeb7e84af923483cfd60a9ba6694df3dd4373
ajsuaa <22391@wc.school.nz>
2023-04-27 22:30:02 +1200

    Thursday  27 April
    Game crashed, restart again

---
+++ Assessment Project 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The camera follows the player's position
+        transform.position = player.transform.position + new Vector3(3, 15, 2);
+    }
+}
